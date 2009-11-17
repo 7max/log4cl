4be3ce7a2d5a252dae08cae730ec62c8cb8c1679
Max Mikhanosha <max@momoland.openchat.com>
2009-11-17 14:53:19 -0500

    (log-sexp): Utility macro for easy logging of variables and sexps

--- cl-log-util.lisp
+++ cl-log-util.lisp
@@ -436,13 +436,42 @@ the logger name would be just package
       (let ((log-macro-name (intern (format nil "LOG-~a" level)))
             (level-name (intern (format nil "+LOG-LEVEL-~a+" level))))
         (push `(defmacro ,log-macro-name (&rest args &environment env)
-                 "Logs the message with the logger. See log-debug for description of arguments"
+                 "Logs the message with the logger.
+
+Synopsis:
+
+ (defun foo ()
+   ;; DEBUG - cl-user.foo: entered
+   (log-debug \"entered\")
+
+   ;; DEBUG - cl-user.bar: message
+   (log-debug :bar \"message\")
+
+   ;; INFO - full.logger.name: message
+   (log-info :/full.logger.name \"message\")
+
+   ;; DEBUG - cl-user.foo.test: entered
+   ;; DEBUG - cl-user.foo.test: arg1=1 arg2=2
+   (flet ((test (arg1 arg2)
+            (log-debug \"enter\")
+            (log-debug arg1 arg2)))
+     (test 1 2)))
+
+Logger category is found by examinining the first argument of the macro.
+
+If first argument is a string then logger name is automatically determined
+in a implementation dependend manner. On implementations that support it
+the logger name would be package.block-name. On implementations where
+it's not possible to determine the block name that is currently being compiled
+the logger name would be just package.
+
+"
                  (log-with-level env ,level-name args))
               list)))
     `(progn
        ,@(nreverse list))))
 
-(deflog-macros fatal error warn info user1 user2 user3 user4 
+(deflog-macros debug fatal error warn info user1 user2 user3 user4 
                trace user5 user6 user7 user8 user9)
 
 (defun log-with-level (env level args)
@@ -486,9 +515,20 @@ the logger name would be just package
       ((keywordp arg)
        (values (logger-name-from-symbol arg env)
                (rest args)))
-      ;; can't determine auto logger name
       (t
        (error "(maybe-auto-logger-name): Unable to figure out type of log statement")))))
+
+
+(defmacro log-sexp (&rest args) 
+  (let ((format 
+         (with-output-to-string (*standard-output*)  
+           (let ((first t))
+             (dolist (arg args)
+               (unless first
+                 (write-string " "))
+               (setf first nil)
+               (format t "~s=~~s" arg))))))
+    `(log-debug ,format ,@args)))
 
 (defun substr (seq from &optional (to (length seq)))
   (declare (fixnum from to)
